c8149095af96d98dd26d5bd887dd25ef0131a19e
NJohnsonCR <60128330+NJohnsonCR@users.noreply.github.com>
2020-02-26 19:58:46 -0600

    Add files via upload

--- Tarea JavaFX/src/sample/Controller.java
+++ Tarea JavaFX/src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import javafx.scene.control.*;
@@ -20,5 +21,24 @@ public class Controller {
         Message = textClient.getText();
         chatClient.appendText(Message + "\n");
         textClient.clear();
+    }
+    public class SocketCliente{
+
+        public void MandarSocket(){
+            Socket socket_client;
+            {
+                try {
+                    socket_client = new Socket("122.0.0.1", 6969);
+
+                    DataOutputStream salida_socket = new DataOutputStream(socket_client.getOutputStream());
+
+                    salida_socket.writeUTF(chatClient.getText());
+
+                    salida_socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
